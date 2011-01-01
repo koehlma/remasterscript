89e16b8384cb459c5935c299c6be8648f67227c2
maximilian <linuxmaxi@online.de>
2011-01-01 22:03:00 +0100

    debian packet building script adjusted

--- build-debian.sh
+++ build-debian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p debian/usr/share/remasterscript/
-cp -rp remasterscript remaster.py debian/usr/share/remasterscript/
-dpkg -b ./debian remasterscript.deb
+cp -rp src/remasterscript/ src/remaster.py debian/usr/share/remasterscript/
+dpkg -b debian/ remasterscript.deb
 
