80b0590143daed29df5bf811f91d49c8b7126aa9
Maximilian Köhl <linuxmaxi@online.de>
2011-01-11 18:18:03 +0100

    fix some debian stuff

--- misc/build-debian.sh
+++ misc/build-debian.sh
@@ -7,12 +7,14 @@ if [ $? = 0 ]; then
 	mkdir -p ./debian/DEBIAN/
 	mkdir -p ./debian/opt/remasterscript/{bin,share}
 	mkdir -p ./debian/usr/share/{pixmaps,applications}/
+	mkdir -p ./debian/usr/bin
 	cp -rp ./src/* debian/opt/remasterscript/share/
 	cp ./icon/icon.png ./debian/usr/share/pixmaps/remasterscript.png
 	cp ./misc/remasterscript.desktop ./debian/usr/share/applications
 	cp ./misc/extract_compressed_fs.static ./debian/opt/remasterscript/bin/extract_compressed_fs
 	cp ./misc/create_compressed_fs.static ./debian/opt/remasterscript/bin/create_compressed_fs
 	cp ./misc/debian.control ./debian/DEBIAN/control
+	cp ./misc/remasterscript ./debian/usr/bin/
 	dpkg -b debian/ remasterscript.deb
 else
 	echo "You have to install dpkg..."
